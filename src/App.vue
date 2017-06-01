61743260693cd0a563868c2db4ef60874bfad204
JHercules <JHercules_qz@qq.com>
2017-06-02 07:35:11 +0800

    add Router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <div class="header">
+      <ul>
+        <li>
+          <router-link to="/">Page1</router-link>
+        </li>
+        <li>
+          <router-link to="/page2">Page2</router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="content">
+      <router-view></router-view>
+    </div>
+    <div class="footer"></div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style>
@@ -19,5 +31,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.side-fade-enter-acvite{
+  transition: all .3s ease;
+}
+.side-fade-enter, .side-fade-leave-acvite{
+  transform: translateX(-430px);
+  opacity: 0;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Page1 from '@/components/page1/Page1'
+import Page2 from '@/components/page2/Page2'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+  mode: 'history',
+  linkActiveClass: 'active',
+  routes: [{
+    path: '/',
+    name: 'page1',
+    component: Page1
+  }, {
+    path: '/page2',
+    name: 'page2',
+    component: Page2
+  }]
 })
