3c4754b9195d2cfac9b0ad65079d4bc68f6d7261
JHercules <JHercules_qz@qq.com>
2017-06-04 13:24:52 +0800

    update Page2

--- src/components/page2/Page2.vue
+++ src/components/page2/Page2.vue
@@ -1,11 +1,53 @@
 <template>
   <div class="page2">
-    page2
+    <p>page2</p>
+    <div>{{ divHtml }}</div>
+    <div v-text="vText"></div>
+    <div v-html="vHtml"></div>
+    <div v-if="vIf">v-if</div>
+    <ul>
+      <li v-for="item in vFor">
+        <p v-text="item.text" v-on:click="doSth"></p>
+      </li>
+    </ul>
+    <input type="text" v-model="vWatch"/>
   </div>
 </template>
 
 <script>
-
+  import Bus from './../../bus.js';
+  export default {
+    name: 'module1',
+    data () {
+      return {
+        divHtml: 'divHtml',
+        vText: 'vText',
+        vHtml: 'vHtml',
+        vIf: false,
+        vFor: [{
+          text:1
+        }, {
+          text:2
+        }],
+        vWatch: "vWatch"
+      }
+    },
+    methods:{
+      doSth: function(){
+        console.log("[Module1.doSth]" + this.divHtml);
+        this.vIf = true;
+        Bus.$emit('imsg1', {a:1, b:2});
+      }
+    },
+    watch:{
+      'vWatch':function (strVal, strOldVal) {
+        console.log("[Module1.Watch.vWatch]" + strVal + "," + strOldVal);
+      },
+      'vIf': function (bVal, bOldVal) {
+        console.log("[Module1.Watch.vIf]" + bVal + "," + bOldVal);
+      }
+    }
+  }
 </script>
 
 <style>
