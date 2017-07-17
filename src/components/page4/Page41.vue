695216811a8dfd0f70d023f26361afa4570dcb55
JHercules <JHercules_qz@qq.com>
2017-07-17 17:54:53 +0800

    重构Page41.vue

--- src/components/page4/Page41.vue
+++ src/components/page4/Page41.vue
@@ -32,7 +32,7 @@
   import MarvelLeaflet from "@/walle/widget/leaflet/MarvelLeaflet";
   import MarvelSwitch from "@/walle/widget/select/MarvelSwitch";
   import MarvelGrid from "@/walle/widget/grid/MarvelGrid";
-  import MarvelWarning from "../../walle/widget/warning/MarvelWarning";
+  import MarvelWarning from "@/walle/widget/warning/MarvelWarning";
   export default {
     components: {
       MarvelWarning,
@@ -44,6 +44,10 @@
     name: 'page41',
     data: function() {
       return {
+        //region const
+        debug: true,
+        timerInterval: 2000,
+        //endregion
         //region crumb
         crumbItems: [{
           label: "设备监控",
@@ -60,25 +64,16 @@
         //region devLst
         devLst: [],
         //endregion
-        //region statusLst
-        statusLst: [],
-        //endregion
         //region warnPanel
         warnPanel: [],
         //endregion
         //region grid
         titles: [{
           label: "ID",
-          width: "5%"
-        }, {
-          label: "名称",
-          width: "5%"
-        }, {
-          label: "描述",
           width: "15%"
         }, {
-          label: "制造时间",
-          width: "5%"
+          label: "名称",
+          width: "15%"
         }, {
           label: "状态",
           width: "5%"
@@ -87,31 +82,16 @@
           width: "5%"
         }, {
           label: "最近更新时间",
-          width: "10%"
-        }, {
-          label: "买家公司",
-          width: "10%"
-        }, {
-          label: "买家地址",
-          width: "5%"
-        }, {
-          label: "是否锁定",
-          width: "5%"
-        }, {
-          label: "所在城市",
-          width: "10%"
-        }, {
-          label: "所在部门",
-          width: "5%"
-        }, {
-          label: "绑定手机11",
-          width: "5%"
+          width: "20%"
         }, {
           label: "经度",
-          width: "5%"
+          width: "10%"
         }, {
           label: "纬度",
-          width: "5%"
+          width: "10%"
+        }, {
+          label: "备注",
+          width: "20%"
         }],
         rows: [],
         //endregion
@@ -127,30 +107,32 @@
       //1.1.init gis map
       this.$refs.refGISMap.init(51.505, -0.09, 13, 18, "/static/leaflet/images/shit.png");
       //1.2.getDevLst
-      this._getDevLstMock();
-      //1.3.draw
-      this._drawGisMap();
+      this._getDevLstMock(function(){
+        //1.3.draw
+        self._drawGisMap();
 
-      //2.warnPanel
-      this._updateWarnPanel();
-
-      //3.grid
-      this._drawGrid();
-
-      //3.getStatus by timer
-      this.timer = setInterval(function(){
-        //3.1.getStatusLst
-        self._getStatusLstMock();
-
-        //3.2.merge
-        self._mergeStatus2DevLst();
-
-        //3.3.draw
-        self._updateGisMap();
-
-        //3.4.update warnPanel
+        //1.4.warnPanel
         self._updateWarnPanel();
-      }, 2000);
+
+        //1.5.grid
+        self._drawGrid();
+      });
+
+      //2.getStatus by timer
+      //2.1.timer
+      this.timer = setInterval(function(){
+        //2.2.getDevLst
+        self._getDevLstMock(function(){
+          //2.3.draw
+          self._updateGisMap();
+
+          //2.4.update warnPanel
+          self._updateWarnPanel();
+
+          //2.5.grid
+          self._drawGrid();
+        });
+      }, this.timerInterval);
     },
     destroyed: function(){
       if(this.timer != undefined){
@@ -172,90 +154,50 @@
       onGisMapClick: function(oPoint){
         console.log(oPoint);
       },
-      _getDevLstMock: function(){
+      _getDevLstMock: function(oCallback){
         this.devLst = [];
-        for(var i=0;i<10;i++){
-          var oDev = {
-            id: i,
-            name: "3217030228" + i,
-            warn: i % 2 ==0 ? true: false,
-            status: "加工",
-            laser: "Raycus-500w",
-            x: 51.5 + Math.random() * 0.1,
-            y: -0.09 + Math.random() * 0.1,
-            desc: "",
-            createTime: "",
-            lastUpdateTime: "",
-            company: "",
-            addr: "",
-            lock: "",
-            city: "",
-            department: "",
-            phone: ""
-          };
-          this.devLst.push(oDev);
+
+        if(this.debug){
+          for(var i=0;i<10;i++){
+            var oDev = {
+              id: i,
+              clientNo: "client1",
+              devId: "3217030228" + i,
+              x: 51.5 + Math.random() * 0.1,
+              y: -0.09 + Math.random() * 0.1,
+              lastUpdateTime: "2017-07-14 10:00:00.540",
+              status: Math.random() > 0.5 ? "加工": "离线",
+              hasWarn: Math.random() > 0.5 ? true: false,
+              warnCount: (Math.random() * 10).toFixed(0)
+            };
+            this.devLst.push(oDev);
+          }
+          oCallback();
+        }
+        else{
+          this.$http.post('/getDevLst', {
+            reqBuVoStr: JSON.stringify({clientNo:"client1"})
+          }).then(res=>{
+            this.devLst = res.data.resultObj;
+            oCallback();
+          });
         }
       },
       _drawGisMap: function(){
         for(var i=0;i<this.devLst.length;i++){
           var oDev = this.devLst[i];
-          var strImgUrl = oDev.warn ?
+          var strImgUrl = false == oDev.hasWarn ?
             "/static/leaflet/images/dev1.png" :
             "/static/leaflet/images/dev2.png";
-          var strTips = "<b>" + oDev.name + "</b><br>" +
+          var strTips = "<b>" + oDev.devId + "</b><br>" +
             oDev.status + "<br>" +
-            oDev.laser + "<br>";
+            oDev.warnCount + "<br>";
           this.$refs.refGISMap.addIcon(oDev.id, oDev.x, oDev.y, strImgUrl, strTips, oDev,
             this._onDBClickGisMap);
         }
       },
       _onDBClickGisMap: function(strId){
         this.$router.push({name: "page42", query: { id: strId }});
-      },
-      _getStatusLstMock: function(){
-        this.statusLst = [];
-        for(var i=0;i<10;i++){
-          var oStatus = {
-            id: i
-          };
-          var iStatus = Math.random();
-          if(iStatus > 0 && iStatus <= 0.3){
-            oStatus.status = "加工";
-          }
-          else if(iStatus > 0.3 && iStatus <= 0.6){
-            oStatus.status = "待机";
-          }
-          else {
-            oStatus.status = "离线";
-          }
-          oStatus.warn = Math.random() > 0.5 ? true : false;
-          this.statusLst.push(oStatus);
-        }
-      },
-      _mergeStatus2DevLst: function(){
-        for(var i=0;i< this.statusLst.length;i++){
-          var oStatus = this.statusLst[i];
-          for(var j=0;j<this.devLst.length;j++){
-            var oDev = this.devLst[j];
-            if(oDev.id == oStatus.id){
-              oDev.status = oStatus.status;
-              oDev.warn = oStatus.warn;
-              break;
-            }
-          }
-        }
-      },
-      _updateGisMap: function(){
-        for(var i=0;i<this.devLst.length;i++){
-          var oDev = this.devLst[i];
-          var strImgUrl = oDev.warn ?
-            "/static/leaflet/images/dev1.png" :
-            "/static/leaflet/images/dev2.png";
-          var strTips = "<b>" + oDev.name + "</b><br>" +
-            oDev.status + "<br>" +
-            oDev.laser + "<br>";
-          this.$refs.refGISMap.updateIcon(oDev.id, strImgUrl, strTips, oDev);
-        }
       },
       _updateWarnPanel: function(){
         var iCountStatus1 = 0;
@@ -274,7 +216,7 @@
             iCountStatus3++;
           }
 
-          if(oDev.warn){
+          if(oDev.hasWarn){
             iCountWarn++;
           }
         }
@@ -295,25 +237,30 @@
       },
       _drawGrid: function(){
         this.rows = [];
-        for(var i=0;i<10;i++){
+        for(var i=0;i<this.devLst.length;i++){
           var oDev = this.devLst[i];
           var oRow = [];
-          oRow.push({value: oDev.id, type:"text"});
-          oRow.push({value: oDev.name, type:"text"});
-          oRow.push({value: oDev.desc, type:"text"});
-          oRow.push({value: oDev.createTime, type:"text"});
+          oRow.push({value: oDev.devId, type:"text"});
+          oRow.push({value: oDev.devId, type:"text"});
           oRow.push({value: oDev.status, type:"text"});
-          oRow.push({value: oDev.warn, type:"text"});
+          oRow.push({value: oDev.warnCount, type:"text"});
           oRow.push({value: oDev.lastUpdateTime, type:"text"});
-          oRow.push({value: oDev.company, type:"text"});
-          oRow.push({value: oDev.addr, type:"text"});
-          oRow.push({value: oDev.lock, type:"text"});
-          oRow.push({value: oDev.city, type:"text"});
-          oRow.push({value: oDev.department, type:"text"});
-          oRow.push({value: oDev.phone, type:"text"});
           oRow.push({value: oDev.x, type:"text"});
           oRow.push({value: oDev.y, type:"text"});
+          oRow.push({value: oDev.desc, type:"text"});
           this.rows.push(oRow);
+        }
+      },
+      _updateGisMap: function(){
+        for(var i=0;i<this.devLst.length;i++){
+          var oDev = this.devLst[i];
+          var strImgUrl = false == oDev.hasWarn ?
+            "/static/leaflet/images/dev1.png" :
+            "/static/leaflet/images/dev2.png";
+          var strTips = "<b>" + oDev.devId + "</b><br>" +
+            oDev.status + "<br>" +
+            oDev.warnCount + "<br>";
+          this.$refs.refGISMap.updateIcon(oDev.id, strImgUrl, strTips, oDev);
         }
       }
     }
