1c64e2c4b259aaf454d6555361c529fe137d6ef6
JHercules <JHercules_qz@qq.com>
2017-07-30 09:08:18 +0800

    add Page40

--- tools/changeDemo.bat
+++ tools/changeDemo.bat
@@ -6,4 +6,7 @@ copy "%~dp0..\src\AppDemo.vue" "%~dp0..\src\App.vue"
 echo copy index.js
 copy "%~dp0..\src\router\indexDemo.js" "%~dp0..\src\router\index.js"
 
+echo copy index.html
+copy "%~dp0..\indexDemo.html" "%~dp0..\index.html"
+
 pause>null
