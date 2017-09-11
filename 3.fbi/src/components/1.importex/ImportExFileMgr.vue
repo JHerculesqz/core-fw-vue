2d9b3061a6127bc5485836fd4745b38ff4d737ad
JHercules <JHercules_qz@qq.com>
2017-09-11 11:59:45 +0800

    O改S业务文件夹创建

---
+++ 3.fbi/src/components/1.importex/ImportExFileMgr.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    ImportEx
+  </div>
+</template>
+
+<script>
+  import MarvelFrame from "@/walle/widget/frame/MarvelFrame";
+  export default {
+    components: {MarvelFrame},
+    name: 'ImportEx',
+    data: function() {
+      return {
+
+      }
+    },
+    mounted: function(){
+      var self = this;
+
+
+    },
+    destroyed: function(){
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
