cb36ee0a4e4e1c3008774f892901ff2bd2034b62
JHercules <JHercules_qz@qq.com>
2017-10-09 10:26:06 +0800

    update fui

--- z.tools/update.bat
+++ z.tools/update.bat
@@ -1,23 +1,6 @@
 echo off
 
-echo xcopy package.json
-xcopy %~dp0..\1.platform\package.json %~dp0..\2.shield\package.json /y /i
-xcopy %~dp0..\1.platform\package.json %~dp0..\2.shield-ins\package.json /y /i
-xcopy %~dp0..\1.platform\package.json %~dp0..\3.fbi\package.json /y /i
-
-echo xcopy index.html
-xcopy %~dp0..\1.platform\index.html %~dp0..\2.shield\index.html /y /i
-xcopy %~dp0..\1.platform\index.html %~dp0..\2.shield-ins\index.html /y /i
-xcopy %~dp0..\1.platform\index.html %~dp0..\3.fbi\index.html /y /i
-
-echo xcopy static
-xcopy %~dp0..\1.platform\static %~dp0..\2.shield\static\ /s /y
-xcopy %~dp0..\1.platform\static %~dp0..\2.shield-ins\static\ /s /y 
-xcopy %~dp0..\1.platform\static %~dp0..\3.fbi\static\ /s /y 
-
-echo xcopy walle
-xcopy %~dp0..\1.platform\src\walle %~dp0..\2.shield\src\walle\ /s /y
-xcopy %~dp0..\1.platform\src\walle %~dp0..\2.shield-ins\src\walle\ /s /y 
-xcopy %~dp0..\1.platform\src\walle %~dp0..\3.fbi\src\walle\ /s /y 
+echo STEP1.xcopy walle
+xcopy %~dp0..\1.platform\src\walle %~dp0..\0.core\src\walle\ /s /y
 
 pause>null
