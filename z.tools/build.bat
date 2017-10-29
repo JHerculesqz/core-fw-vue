64b850ade798e704dc4b607e690f29b47462771f
JHercules <JHercules_qz@qq.com>
2017-10-29 09:25:03 +0800

    update gis

--- z.tools/build.bat
+++ z.tools/build.bat
@@ -2,6 +2,7 @@ echo off
 
 echo STEP1.xcopy 1.platform.gis
 xcopy %~dp0..\1.platform.gis %~dp0..\1.platform\static\gis\ /s /y
+rd /s /Q %~dp0..\1.platform\static\gis\image\
 
 echo STEP2.xcopy 1.platform.topo
 xcopy %~dp0..\1.platform.topo %~dp0..\1.platform\static\topo\ /s /y
