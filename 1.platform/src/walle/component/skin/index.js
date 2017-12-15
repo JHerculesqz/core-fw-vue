6208b999f59b16a020518fe6ec65f9b9ce41bdc7
JHercules <JHercules_qz@qq.com>
2017-12-15 10:32:31 +0800

    update skins

--- 1.platform/src/walle/component/skin/index.js
+++ 1.platform/src/walle/component/skin/index.js
@@ -2,9 +2,15 @@ export default {
   themeChange: function (strTheme) {
     //1.get current theme
     var strClassName = document.body.className;
-    //2.kill me
-    document.body.classList.remove(strClassName);
+
+    if(strClassName){
+      //2.kill me
+      document.body.classList.remove(strClassName);
+    }
+
     //3.change new girl friend
-    document.body.classList.add(strTheme);
+    if(strTheme){
+      document.body.classList.add(strTheme);
+    }
   }
 }
